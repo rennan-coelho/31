52094c484f56250c089cabe4be3683772f35c563
Rennan Coelho <141645170+rennan-coelho@users.noreply.github.com>
2024-03-30 20:46:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ var config = {
         "apesar de ele não lhe merecer",
         "você é incrível!!!",
         "jamais medirei palavras para lhe elogiar",
+        "ou para lhe descrever",
         "obrigado por fazer parte da minha vida",
         "eu amo você, muito!",
         "sempre conte comigo",
@@ -18,18 +19,18 @@ var config = {
     ],
    
     imgs: {
-       // "foto": "./imgs/xiaokeai.png",
+       // "foto": "./imgs/birthday.png",
         // "foto": "./imgs/birthday.jpg",
     },
     // Descrição do texto do botão, você pode alterá-lo para o texto que desejar
     desc: {
         turn_on: "Clica aqui cabeçona",
-        play: "que silêncio... que tal uma música?",
-        bannar_coming: "kkkk, sim, é aquela música",
+        play: "Ligue o som",
+        bannar_coming: "e sim, é aquela música",
         balloons_flying: "uma surpresinha...",
         cake_fadein: "acho que merece um bolo",
         light_candle: "acenda a vela, por favor",
-        wish_message: "até que ficou bonito né?",
+        wish_message: "até que ficou legal né?",
         story: "umas palavrinhas...",
     }
 };
