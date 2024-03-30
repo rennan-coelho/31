119fc18a04087378a0283be991dec0729fa08259
Rennan Coelho <rennancoelhocontato@gmail.com>
2024-03-30 20:43:03 -0300

    teste

---
+++ config.js
@@ -0,0 +1,35 @@
+
+var config = {
+   
+    texts: [
+        "E mais uma vez",    
+        "um feliz aniversário lice <3", 
+        "você merece tudo de bom nesse mundo",
+        "apesar de ele não lhe merecer",
+        "você é incrível!!!",
+        "jamais medirei palavras para lhe elogiar",
+        "obrigado por fazer parte da minha vida",
+        "eu amo você, muito!",
+        "sempre conte comigo",
+        "aproveite muito seu dia viu",
+        "um último feliz aniversário <3",
+        "fim",
+        "ou você pode ficar curtindo sua música",
+    ],
+   
+    imgs: {
+       // "foto": "./imgs/xiaokeai.png",
+        // "foto": "./imgs/birthday.jpg",
+    },
+    // Descrição do texto do botão, você pode alterá-lo para o texto que desejar
+    desc: {
+        turn_on: "Clica aqui cabeçona",
+        play: "que silêncio... que tal uma música?",
+        bannar_coming: "kkkk, sim, é aquela música",
+        balloons_flying: "uma surpresinha...",
+        cake_fadein: "acho que merece um bolo",
+        light_candle: "acenda a vela, por favor",
+        wish_message: "até que ficou bonito né?",
+        story: "umas palavrinhas...",
+    }
+};
